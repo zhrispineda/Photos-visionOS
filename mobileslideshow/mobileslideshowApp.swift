a7ac5ee605dd6bb9cde3683960947c0573af8819
Chris <zhris@zhr.is>
2024-07-02 12:58:32 -0400

    Initial Commit

---
+++ mobileslideshow/mobileslideshowApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileslideshowApp.swift
+//  mobileslideshow
+//
+//  Created by Chris on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct mobileslideshowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
